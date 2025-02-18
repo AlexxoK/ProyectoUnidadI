5b41d60cdfd543ef2004a5e2ba106b261190bb25
AlexxoK <dmonterroso-2023234@kinal.edu.gt>
2025-02-18 11:40:25 -0600

    users realizado y funcional del lado administrador

---
+++ src/helpers/db-validator.js
@@ -0,0 +1,26 @@
+import Role from '../role/role.model.js';
+import User from '../users/user.model.js';
+
+export const esRoleValido = async (role = ' ') => {
+    const existeRol = await Role.findOne({ role });
+
+    if (!existeRol) {
+        throw new Error(`Role ${ role } does not exist in the database!`);
+    }
+}
+
+export const existenteEmail = async (email = ' ') => {
+    const existeEmail = await User.findOne({ email });
+
+    if (existeEmail) {
+        throw new Error(`Email ${ email } exists in the database!`);
+    }
+}
+
+export const existeUsuarioById = async (id = '') => {
+    const existeUsuario = await User.findById(id);
+
+    if (!existeUsuario) {
+        throw new Error(`id ${ id } dont exists!`);
+    }
+}
---
+++ src/helpers/generate-jwt.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+import { token } from 'morgan';
+
+export const generarJWT = (uid = ' ') => {
+    return new Promise((resolve, reject) => {
+
+        const payload = { uid };
+
+        jwt.sign(
+            payload,
+            process.env.SECRETORPRIVATEKEY,
+            {
+                expiresIn: '1h'
+            },
+            (err, token) => {
+                err ? (console.log(err), reject('Token not generated!')) : resolve(token);
+            }
+        );
+    });
+}
